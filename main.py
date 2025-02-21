2514878eb82bdcc798693d6e3dabc77d74877e0c
Kangaroo6991 <luungockhang@gmail.com>
2025-02-21 21:32:36 +0000

    Updated main.py and added stats.py so it can now take book as a CLI argument

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import sys
+from stats import get_num_words
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) == 1:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    
+    
+
     text = get_book_text(book_path)
     # print(text)
-    word_count = get_word_count(text)
+    word_count = get_num_words(text)
     # print(f"There are {word_count} words in this text")
     character_count = count_characters(text)
     #print(character_count)
@@ -16,9 +25,7 @@ def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
 
-def get_word_count(text):
-    word_count = len(text.split())
-    return word_count
+
 
 def count_characters(text):
     lowered_text = text.lower()
@@ -49,8 +56,8 @@ def convert_char_dictionary_to_list(character_count):
 
 def print_report(book_path, word_count, sorted_list_of_chars):
     print(f"===== Begin report of {book_path} =====")
-    print(f"{word_count} found in the document")
+    print(f"{word_count} words found in the document")
     for char_entry in sorted_list_of_chars:
-        print(f"The '{char_entry["char"]}' was found {char_entry["num"]} times")
+        print(f"'{char_entry["char"]}: {char_entry["num"]}")
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text):
+    word_count = len(text.split())
+    return word_count
